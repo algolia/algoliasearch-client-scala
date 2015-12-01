6aa3857983b5cddfc31a903a9d0bb5283cdf596a
Rémy-Christophe Schermesser <git@schermesser.com>
2015-12-01 15:54:56 +0100

    Stop calls if a server returns a 4XX
    Closes #1

--- src/main/scala/algolia/DispatchHttpClient.scala
+++ src/main/scala/algolia/DispatchHttpClient.scala
@@ -1,11 +1,15 @@
 package algolia
 
 import algolia.http._
-import dispatch.Defaults._
+import com.ning.http.client.Response
 import dispatch._
+import dispatch.as.json4s._
+import org.json4s._
 
 private[algolia] trait DispatchHttpClient {
-  implicit val formats = org.json4s.DefaultFormats
+  implicit val formats = DefaultFormats
+
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   def request[T: Manifest](host: String, headers: Map[String, String], payload: HttpPayload): Future[T] = {
     val path = payload.path.foldLeft(url(host).secure) {
@@ -27,9 +31,23 @@ private[algolia] trait DispatchHttpClient {
       request = request << payload.body.get
     }
 
-    Http(request OK dispatch.as.json4s.Json).map(_.extract[T])
+    val responseManager: Response => T = { response =>
+      response.getStatusCode / 100 match {
+        case 2 => Json(response).extract[T]
+        case 4 => throw `4XX`(response.getStatusCode, (Json(response) \ "message").extract[String])
+        case _ => throw UnexpectedResponse(response.getStatusCode)
+      }
+    }
+
+    Http(request > responseManager)
   }
 
 }
+
+case class `4XX`(code: Int, message: String) extends Exception("Failure \"%s\", response status: %d".format(message, code))
+
+case class UnexpectedResponse(code: Int) extends Exception("Unexpected response status: %d".format(code))
+
+private[algolia] case class `4XXResponse`(message: String)
 
 private[algolia] object DispatchHttpClient extends DispatchHttpClient
