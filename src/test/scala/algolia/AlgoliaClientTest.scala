95c1f10047cb1da639e2d6ce0a9199a1039d6b49
Anthony Seure <anthony.seure@gmail.com>
2020-06-18 12:18:06 +0200

    test: prevent user-agent test from failing on JDK >8

--- src/test/scala/algolia/AlgoliaClientTest.scala
+++ src/test/scala/algolia/AlgoliaClientTest.scala
@@ -58,7 +58,7 @@ class AlgoliaClientTest extends AlgoliaTest {
 
     it("should set user agent") {
       apiClient.userAgent should (startWith("Algolia for Scala (1.") and include(
-        "; JVM (1.8"
+        "; JVM (1"
       ) and include("; Scala (2.1"))
     }
 
