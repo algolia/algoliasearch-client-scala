755591b1103ba16f5e5008e26388343850ab7b3d
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-09-23 17:31:25 +0200

    get recommendation definition

--- src/main/scala/algolia/definitions/GetRecommendationDefinition.scala
+++ src/main/scala/algolia/definitions/GetRecommendationDefinition.scala
@@ -1,25 +1,28 @@
 package algolia.definitions
 
 import algolia.http.{GET, HttpPayload}
-import algolia.objects.RequestOptions
+import algolia.objects.{RecommendationsOptions, RequestOptions}
 import org.json4s.Formats
+import org.json4s.native.Serialization.write
 
-case class GetRecommendDefinition(
+case class GetRecommendationDefinition(
+    query: RecommendationsOptions,
     requestOptions: Option[RequestOptions] = None
 )(implicit val formats: Formats)
     extends Definition {
 
-  override type T = GetRecommendDefinition
+  override type T = GetRecommendationDefinition
 
   override def options(
       requestOptions: RequestOptions
-  ): GetRecommendDefinition =
+  ): GetRecommendationDefinition =
     copy(requestOptions = Some(requestOptions))
 
   override private[algolia] def build(): HttpPayload = {
     HttpPayload(
       GET,
       Seq("1", "indexes", "*", "recommendations"),
+      body = Some(write(query)),
       isSearch = true,
       requestOptions = requestOptions
     )
